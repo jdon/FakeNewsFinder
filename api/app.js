a9ab5380bc022641df42f19320936538d95947b1
Alexander Kalinin <kalininator@gmail.com>
2017-03-12 16:59:54 +0000

    returns random score
    eg: fake.jdon.uk/url/www.google.com to call it

--- api/app.js
+++ api/app.js
@@ -6,5 +6,11 @@ var fs = require('fs');
 app.get('/', function(req,res) {
 	res.send(fs.readFileSync(__dirname+'/index.html','utf8'));
 });
+app.get('/url/:url', function(req,res){
+	
+	var score = Math.random()*5;
+	score = Math.round(score);
+	res.send({url:req.params.url,score:score});
+});
 
 app.listen(port);
