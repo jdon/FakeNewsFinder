a6fd1a5a21b61e7d150061bd4aafb27b23acfe86
Jonathan Donaldson <jdonaldson278@gmail.com>
2017-03-13 01:18:02 +0000

    Added NewsParser

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 var port = process.env.PORT || 3000;
 var express = require('express');
+var parser = require('./NewsParser');
 var app = express();
 var fs = require('fs');
 
@@ -7,10 +8,19 @@ app.get('/', function(req,res) {
 	res.send(fs.readFileSync(__dirname+'/index.html','utf8'));
 });
 app.get('/url/:url', function(req,res){
-	
-	var score = Math.random()*5;
-	score = Math.round(score);
-	res.send({url:req.params.url,score:score});
+    var url = req.params.url;
+    var score = Math.random()*5;
+    score = Math.round(score);
+    parser.parse(url,function(error,result)
+    {
+        if(error)
+        {
+            console.log("error");
+            res.send({url:url,score:score,js:"error"})
+        }
+        res.send({url:url,score:score,result:result});
+
+    });
 });
 
 app.listen(port);
