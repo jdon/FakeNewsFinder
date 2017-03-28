f7176f1db82fe52a6c11bd1850e43ee23a5edfe2
Jonathan Donaldson <jdonaldson278@gmail.com>
2017-03-28 16:00:52 +0100

    Updated for aws api

--- api/app.js
+++ api/app.js
@@ -1,8 +1,14 @@
 var port = process.env.PORT || 3000;
 var express = require('express');
 var parser = require('./NewsParser');
+var dynDb = require('./Database');
 var app = express();
 var fs = require('fs');
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new AYLIENTextAPI({
+    application_id: "de542d92",
+    application_key: "2ad183766257e84750947020a62d0821"
+});
 
 app.get('/', function(req,res) {
 	res.send(fs.readFileSync(__dirname+'/index.html','utf8'));
@@ -37,7 +43,23 @@ app.get('/url/:url', function(req,res){
             var content = result.content;
             var excerpt = result.excerpt;
             var leadImageURL = result.lead_image_url;
-            console.log(title + author + url + domain + excerpt + leadImageURL);
+            //console.log(title + author + url + domain + excerpt + leadImageURL);
+/*            textapi.summarize({
+                'url': url,
+                'language': "en"
+            }, function(error, response) {
+                if (error === null) {
+                    console.log(response);
+                }else
+                    {
+                        console.log(error);
+                    }
+            });*/
+            dynDb.put(url,domain,title,author,excerpt,function(error,result)
+            {
+                if (error) console.log(error, error.stack); // an error occurred
+                else     console.log(result);           // successful response
+            });
             res.send({url:url,domain:domain,title:title,author:author,excerpt:excerpt,leadImageURL:leadImageURL});
         }
 
