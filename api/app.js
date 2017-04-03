38afffe007cc047fb887cf007ee2e9a5270eb2f5
Alexander Kalinin <kalininator@gmail.com>
2017-04-03 21:24:38 +0100

    Allow CORS test

--- api/app.js
+++ api/app.js
@@ -11,6 +11,12 @@ var fs = require('fs');
 var useDB = true;
 if(useDB) dynDb = require('./Database');
 
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.get('/', function(req,res) {
     res.send(fs.readFileSync(__dirname+'/index.html','utf8'));
 });
